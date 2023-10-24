0c171a514b0b2f69f53d5d7984b181f43bf73e32
Shvec Sergei <fejanin@mail.ru>
2023-10-24 20:28:30 +0300

    Определение моделей. Миграции: создание и выполнение

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from coolsite import settings
 from robots.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('robots.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # хэндлеры работают только когда DEBUG = False
 handler404 = pageNotFound # строку не менять!!!
